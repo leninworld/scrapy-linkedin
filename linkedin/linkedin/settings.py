3f6eb3a5f018685848671d1226c2135874b3e9bc
Wei Chen <cleverdie@gmail.com>
2012-10-24 17:57:15 +0800

    create scrapy project

---
+++ linkedin/linkedin/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for linkedin project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'linkedin'
+
+SPIDER_MODULES = ['linkedin.spiders']
+NEWSPIDER_MODULE = 'linkedin.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'linkedin (+http://www.yourdomain.com)'
