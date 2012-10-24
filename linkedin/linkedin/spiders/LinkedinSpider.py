f798a9a96914569248f7f6a042e198a81bb423ab
Wei Chen <cleverdie@gmail.com>
2012-10-24 19:44:19 +0800

    add basic url detection and parsing

--- linkedin/linkedin/spiders/LinkedinSpider.py
+++ linkedin/linkedin/spiders/LinkedinSpider.py
@@ -1,7 +1,9 @@
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy import log
 from linkedin.items import LinkedinItem
+from os import path
 
 class LinkedinspiderSpider(CrawlSpider):
     name = 'LinkedinSpider'
@@ -39,10 +41,89 @@ class LinkedinspiderSpider(CrawlSpider):
         #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse(self, response):
+        """
+        default parse method, rule is not useful now
+        """
         hxs = HtmlXPathSelector(response)
+        index_level = self.determine_level(response)
+        if index_level == 1:
+            # get second level urls to crawl
+            self.save_to_file_system(index_level, response)
+            pass
+        elif index_level == 2:
+            # get third level urls to crawl
+            pass
+        elif index_level == 3:
+            pass
+        elif index_level == 4:
+            pass
         i = LinkedinItem()
         #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
         #i['name'] = hxs.select('//div[@id="name"]').extract()
         #i['description'] = hxs.select('//div[@id="description"]').extract()
         return i
+    
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        index_level = self.determine_level(response)
+        if index_level == 1:
+            # get second level urls to crawl
+            self.save_to_file_system(index_level, response)
+            # TODO yield new request
+            pass
+        elif index_level == 2:
+            # get third level urls to crawl
+            # TODO yield new request
+            pass
+        elif index_level == 3:
+            # TODO yield new request
+            pass
+        elif index_level == 4:
+            pass
+        i = LinkedinItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
+
+    
+    def determine_level(self, response):
+        """
+        determine the index level of current response, so we can decide wether to continue crawl or not.
+        first level: people/[a-z].html
+        second level: people/[A-Z][\d+].html
+        third level: people/?.html
+        fourth level: profile link or search link
+        """
+        import re
+        url = response.url
+        if re.match(".+/[a-z]\.html", url):
+            return 1
+        elif re.match(".+/[A-Z]\d+.html", url):
+            return 2
+        elif re.match(".+/people/[a-zA-Z0-9-]+.html", url):
+            return 3
+        # TODO seperate search page and personal profile page.
+        return 4
+    
+    def save_to_file_system(self, level, response):
+        """
+        save the response to related folder
+        """
+        if level in [1,2,3]:
+            fileName = self.get_clean_file_name(level, response)
+            fn = path.join(self.settings["DOWNLOAD_FILE_FOLDER"],"first_level",fileName)
+            self.log("Saving to %s" % fn, level=log.DEBUG)
+            with open(fn,"w") as f:
+                f.write(response.body)
+        elif level == 4:
+            # deal with search page or true personal profile page
+            pass
+    
+    def get_clean_file_name(self, level, response):
+        if level in [1,2,3]:
+            fn = response.url.split("/")[-1]
+            return fn
+        elif level == 4:
+            pass
