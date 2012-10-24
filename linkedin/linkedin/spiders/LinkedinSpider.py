6715bb59b5e4917b2f8b8e26da74434f36670ab7
Wei Chen <cleverdie@gmail.com>
2012-10-24 18:03:28 +0800

    generate spider

---
+++ linkedin/linkedin/spiders/LinkedinSpider.py
@@ -0,0 +1,21 @@
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from linkedin.items import LinkedinItem
+
+class LinkedinspiderSpider(CrawlSpider):
+    name = 'LinkedinSpider'
+    allowed_domains = ['linkedin.com']
+    start_urls = ['http://www.linkedin.com/']
+
+    rules = (
+        #Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        hxs = HtmlXPathSelector(response)
+        i = LinkedinItem()
+        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
+        #i['name'] = hxs.select('//div[@id="name"]').extract()
+        #i['description'] = hxs.select('//div[@id="description"]').extract()
+        return i
